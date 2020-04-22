78707812e2a3b2eabde1bde8702dc24142d9c83b
Ryan Sutrisno <45848684+ryansutrisno@users.noreply.github.com>
2020-04-22 14:02:19 +0700

    delete function and placeholder

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -6,17 +6,25 @@ import Todo from "./Todo";
 const Todos = ({ todos, completeTodo }) => {
   return (
     <section className="todos">
-      {todos.map((todo, index) => {
-        return (
-          <Todo
-            key={index}
-            text={todo.text}
-            isCompleted={todo.isCompleted}
-            completeTodo={completeTodo}
-            index={index}
-          />
-        );
-      })}
+      {todos.length > 0 &&
+        todos.map((todo, index) => {
+          return (
+            <Todo
+              key={index}
+              text={todo.text}
+              isCompleted={todo.isCompleted}
+              completeTodo={completeTodo}
+              index={index}
+            />
+          );
+        })}
+      {todos.length === 0 && (
+        <div className="todo-placeholder-text">
+          Tambahkan todo dengan mengklik tombol{" "}
+          <span className="add-button-placeholder-text">Add</span> di pojok kiri
+          atas
+        </div>
+      )}
     </section>
   );
 };
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -15,9 +15,12 @@ const TodoList = () => {
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = value => {
-    const addedTodo = [...todos, { text: value }];
-
-    setTodos(addedTodo);
+    if (todos.length < 10) {
+      const addedTodo = [...todos, { text: value }];
+      setTodos(addedTodo);
+    } else {
+      alert("Maksimal 10 aja bosque...!");
+    }
   };
 
   const completeTodo = index => {
@@ -27,13 +30,19 @@ const TodoList = () => {
     setTodos(addedTodo);
   };
 
+  const clearTodos = () => setTodos([]);
+
   const showAddToggle = () => setShowAdd(!showAdd);
 
   console.log("todos", todos);
 
   return (
     <Paper>
-      <Header showAddToggle={showAddToggle} showAdd={showAdd} />
+      <Header
+        showAddToggle={showAddToggle}
+        showAdd={showAdd}
+        clearTodos={clearTodos}
+      />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
       <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
